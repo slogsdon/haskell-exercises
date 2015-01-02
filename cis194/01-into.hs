6806450c59edc56fbf9b60e7c0baa9c539a5f3fc
Shane Logsdon <shane.logsdon@e-hps.com>
2015-01-01 23:28:32 -0500

    adding cis194 first solution

---
+++ cis194/01-into.hs
@@ -0,0 +1,42 @@
+module Main where
+
+-- Exercise 1
+toDigits :: Integer -> [Integer]
+toDigits n = splitDigits n []
+  where
+    splitDigits num acc
+      | num <= 0 = acc
+      | otherwise = splitDigits next (digit:acc)
+      where
+        digit = num `mod` 10
+        next  = num `div` 10
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev = reverse . toDigits
+
+-- Exercise 2
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = double [] False
+  where
+    double acc _ []         = acc
+    double acc True (x:xs)  = double ((2*x):acc) False xs
+    double acc False (x:xs) = double (x:acc) True xs
+
+-- Exercise 3
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . concat . map toDigits
+
+-- Exercise 4
+validate :: Integer -> Bool
+validate n = sum `mod` 10 == 0
+  where
+    sum = sumDigits $ doubleEveryOther $ toDigitsRev n
+
+-- Exercise 5
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi n source dest int =
+  hanoi (n - 1) source int dest ++ [(source, dest)] ++ hanoi (n - 1) int dest source
---
+++ cis194/README.md
@@ -0,0 +1,3 @@
+# CIS 194 (Spring 2013)
+
+If you're solving the homework assignments from CIS 194, please complete them before looking at my solutions for your own benefit.
