b7f484ce360d71715b33444f4dc4af318b2c8eb5
Shane Logsdon <shane@shanelogsdon.com>
2015-03-01 22:59:21 -0500

    scheme: evaluation exercise 1

--- write-a-scheme/evaluation-part1/src/Parser/Evaluator.hs
+++ write-a-scheme/evaluation-part1/src/Parser/Evaluator.hs
@@ -3,11 +3,9 @@ module Parser.Evaluator where
 import Parser.Types.LispVal
 
 eval :: LispVal -> LispVal
-eval val@(String _)             = val
-eval val@(Number _)             = val
-eval val@(Bool _)               = val
 eval (List [Atom "quote", val]) = val
 eval (List (Atom f : args))     = apply f $ map eval args
+eval val                        = val
 
 apply :: String -> [LispVal] -> LispVal
 apply f args = maybe (Bool False) ($ args) $ lookup f primitives
@@ -19,7 +17,10 @@ primitives = [("+", numericBinop (+)),
               ("/", numericBinop div),
               ("mod", numericBinop mod),
               ("quotient", numericBinop quot),
-              ("remainder", numericBinop rem)]
+              ("remainder", numericBinop rem),
+              ("symbol?", isAtom),
+              ("string?", isString),
+              ("number?", isNumber)]
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
 numericBinop op params = Number $ foldl1 op $ map unpackNum params
@@ -27,3 +28,24 @@ numericBinop op params = Number $ foldl1 op $ map unpackNum params
 unpackNum :: LispVal -> Integer
 unpackNum (Number n) = n
 unpackNum _          = 0
+
+isAtom :: [LispVal] -> LispVal
+isAtom vals = Bool $ foldl1 (&&) $ map isAtom' vals
+  where
+    isAtom' :: LispVal -> Bool
+    isAtom' (Atom _) = True
+    isAtom' _        = False
+
+isString :: [LispVal] -> LispVal
+isString vals = Bool $ foldl1 (&&) $ map isString' vals
+  where
+    isString' :: LispVal -> Bool
+    isString' (String _) = True
+    isString' _          = False
+
+isNumber :: [LispVal] -> LispVal
+isNumber vals = Bool $ foldl1 (&&) $ map isNumber' vals
+  where
+    isNumber' :: LispVal -> Bool
+    isNumber' (Number _) = True
+    isNumber' _          = False
